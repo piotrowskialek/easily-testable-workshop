22e79a027b9963e76ebecd704303b906b894a121
aleksanderpiotrowski <aleksander.piotrowski2@orange.com>
2020-06-18 11:09:00 +0200

    init

---
+++ src/main/java/com/workshop/easilytestable/EasilyTestableApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.easilytestable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasilyTestableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasilyTestableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop/easilytestable/EasilyTestableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.easilytestable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasilyTestableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
